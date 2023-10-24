1a7f0e23b17fff81b78a335bdaf670dd7f10b5a7
DanielBelovol <danielbelovol08@gmail.com>
2023-10-24 20:38:44 +0300

    finished

--- src/main/java/com/example/tgbot/Bank.java
+++ src/main/java/com/example/tgbot/Bank.java
@@ -13,45 +13,60 @@ import java.net.http.HttpResponse;
 
 public class Bank {
     @SneakyThrows
-    public String usdGet(String uri,String nameBuy,String nameSell, String rateCode, String eq){
+    public String getRate(String value, String bank, String sellOrBuy) {
+        String rateCode = null;
+        String uri = null;
+        String eq = null;
+        String nameBuyOrSell = null;
+
+        if (bank.equalsIgnoreCase("privat")) {
+            uri = "https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5";
+            eq = "ccy";
+
+            if (sellOrBuy.equalsIgnoreCase("sell")) nameBuyOrSell = "sale";
+            else if (sellOrBuy.equalsIgnoreCase("buy")) nameBuyOrSell = "buy";
+
+            if (value.equalsIgnoreCase("usd")) rateCode = "USD";
+            else if (value.equalsIgnoreCase("eur")) rateCode = "EUR";
+        } else if (bank.equalsIgnoreCase("mono")) {
+            uri = "https://api.monobank.ua/bank/currency";
+            eq = "currencyCodeA";
+
+            if (sellOrBuy.equalsIgnoreCase("sell")) nameBuyOrSell = "rateSell";
+            else if (sellOrBuy.equalsIgnoreCase("buy")) nameBuyOrSell = "rateBuy";
+
+            if (value.equalsIgnoreCase("usd")) rateCode = "840";
+            else if (value.equalsIgnoreCase("eur")) rateCode = "978";
+        } else if (bank.equalsIgnoreCase("nby")) {
+            uri = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+            eq = "r030";
+
+            if (sellOrBuy.equalsIgnoreCase("buy")) nameBuyOrSell = "rate";
+            else System.out.println("invalid BuyOrSell");
+
+            if (value.equalsIgnoreCase("usd")) rateCode = "840";
+            else if (value.equalsIgnoreCase("eur")) rateCode = "978";
+        }
+
         HttpRequest request = HttpRequest.newBuilder(new URI(uri))
-                .header("Content-type" , "application/json;charset=UTF-8")
+                .header("Content-type", "application/json;charset=UTF-8")
                 .GET()
                 .build();
         HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         JsonArray jsonArray = JsonParser.parseString(response.body()).getAsJsonArray();
-
-        for (JsonElement element : jsonArray) {
-            JsonObject currency = element.getAsJsonObject();
-            if (rateCode.equals(currency.get(eq).getAsString())) {
-                return currency.get(nameBuy).getAsString() +" "+currency.get(nameSell).getAsString();
+        try {
+            for (JsonElement element : jsonArray) {
+                JsonObject currency = element.getAsJsonObject();
+                if (rateCode.equals(currency.get(eq).getAsString())) {
+                    return currency.get(nameBuyOrSell).getAsString();
+                }
             }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
         }
 
-        return response.body();
+        return null;
     }
-    @SneakyThrows
-    public String euroGet(String uri,String nameBuy,String nameSell, String rateCode, String eq){
-        HttpRequest request = HttpRequest.newBuilder(new URI(uri))
-                .header("Content-type" , "application/json;charset=UTF-8")
-                .GET()
-                .build();
-        HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
-
-        JsonArray jsonArray = JsonParser.parseString(response.body()).getAsJsonArray();
-
-        for (JsonElement element : jsonArray) {
-            JsonObject currency = element.getAsJsonObject();
-            if (rateCode.equals(currency.get(eq).getAsString())) {
-                return currency.get(nameBuy).getAsString() +" "+currency.get(nameSell).getAsString();
-            }
-        }
-
-        return response.body();
-    }
-
-
 }
